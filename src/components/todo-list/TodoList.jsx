76715dd686fb8c887833015ae85b022c8c37a616
Jon Jackson <ocskier@gmail.com>
2023-07-10 19:45:05 -0400

    Adds final solution for deployment

--- src/components/todo-list/TodoList.jsx
+++ src/components/todo-list/TodoList.jsx
@@ -1,34 +1,61 @@
-import ListGroup from "react-bootstrap/ListGroup";
-import { Button, Card, Container } from "react-bootstrap";
+import ListGroup from 'react-bootstrap/ListGroup';
+import { Button, Card, Container } from 'react-bootstrap';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, setTodos }) => {
   // Create 2 functional expressions that will handle delete and mark as done
 
   // Handle delete will create a copy of todos, remove the selected one by id,
-  // and setTodos with the new array copy minus the deleted ones.
+  // and setTodos with the new array copy minus the deleted one.
+  const deleteTodo = (id) => {
+    const copyOfTodos = [...todos];
+    var filtered = copyOfTodos.filter((todo) => {
+      return todo.id !== id;
+    });
+    setTodos(filtered);
+  };
 
   // Handle mark as done will create a copy of todos, loop thru and find the
   // todo by id, assign true to its done property, and setTodos with the new
   // array copy including the edited one.
+  const markAsDone = (id) => {
+    const copyOfTodos = [...todos];
+    var edited = copyOfTodos.map((todo) => {
+      if (todo.id === id) {
+        todo.done = true;
+      }
+      return todo;
+    });
+    setTodos(edited);
+  };
 
   return (
-    <Card style={{ width: "fit-content", margin: "0 auto" }}>
+    <Card style={{ width: 'fit-content', margin: '0 auto' }}>
       <ListGroup>
         {todos.map((todo) => (
           <ListGroup.Item
             className="text-light bg-dark d-flex justify-content-between align-items-center"
             key={todo.id}
           >
-            <p className="pt-3" style={{ minWidth: "16rem" }}>
+            <p className="pt-3" style={{ minWidth: '16rem' }}>
               {todo.id} - {todo.text}
             </p>
-            <Container>
+            <Container className="d-flex justify-content-end">
               {/* Add onClick handlers for deleting and mark as done operations */}
-              <Button className="mx-1" variant="primary">
+              {!todo.done ? (
+                <Button
+                  className="mx-1"
+                  variant="danger"
+                  onClick={() => markAsDone(todo.id)}
+                >
+                  Mark As Done
+                </Button>
+              ) : null}
+              <Button
+                className="mx-1"
+                variant="primary"
+                onClick={() => deleteTodo(todo.id)}
+              >
                 Delete
-              </Button>
-              <Button className="mx-1" variant="danger">
-                Mark As Done
               </Button>
             </Container>
           </ListGroup.Item>
